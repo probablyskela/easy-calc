280e810783cbc169383f2178ccb5711b0b5cf985
VasyaR <90523681+VasyaR@users.noreply.github.com>
2022-10-17 12:58:25 +0300

    Update models.py

--- models.py
+++ models.py
@@ -3,26 +3,26 @@ from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
 
-class User(Base):
+class Users(Base):
 	__tablename__ = "users"
 	
 	id = Column(Integer, Identity(start = 1, cycle = False), primary_key = True, nullable = False)
 	email = Column(String(75), nullable = False)
-	username = Column(String(30), nullable = False)
+	username = Column(String(30), nullable = False, unique=True)
 	role = Column(String(14), nullable = False)
 	
-class Calculator(Base):
+class Calculators(Base):
 	__tablename__ = "calculators"
 	
 	id = Column(Integer, Identity(start = 1, cycle = False), primary_key = True, nullable = False)
 	name = Column(String(40), nullable = False)
 	description = Column(String(256), nullable = True)
 	input_data = Column(String(256), nullable = False) 
-	code = Column(String(256), nullable = False)
+	code = Column(String(256), nullable = False, unique=True)
 	codePrivacy = Column(Boolean, nullable = False)
 	owner_id = Column(Integer, ForeignKey('users.id'), nullable = False)
 
-class Review(Base):
+class Reviews(Base):
 	__tablename__ = "reviews"
 	
 	id = Column(Integer, Identity(start = 1, cycle = False), primary_key = True, nullable = False)
